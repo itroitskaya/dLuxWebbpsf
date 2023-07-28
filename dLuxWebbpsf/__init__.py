738d4080ab4151aa3eb1ea7c7d9ec74989c352a0
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2023-07-28 16:16:30 +1000

    Adding the instruments.py files to package imports

--- dLuxWebbpsf/__init__.py
+++ dLuxWebbpsf/__init__.py
@@ -4,17 +4,22 @@ __version__ = "0.0.1"
 # Import as modules
 from . import optical_layers
 from . import optics
+from . import instruments
 
 # Import core functions from modules
-from .optics         import *
-from .optical_layers       import *
+from .optics import *
+from .optical_layers import *
+from .instruments import *
 
 # Add to __all__
-__all__ = optics.__all__ + optical_layers.__all__ 
+__all__ = optics.__all__ + optical_layers.__all__, instruments.__all__
 
 # Check for 64-bit
 from jax import config
+
 if not config.x64_enabled:
-    print("dLux: Jax is running in 32-bit, to enable 64-bit visit: "
-          "https://jax.readthedocs.io/en/latest/notebooks/"
-          "Common_Gotchas_in_JAX.html#double-64bit-precision")
+    print(
+        "dLux: Jax is running in 32-bit, to enable 64-bit visit: "
+        "https://jax.readthedocs.io/en/latest/notebooks/"
+        "Common_Gotchas_in_JAX.html#double-64bit-precision"
+    )
